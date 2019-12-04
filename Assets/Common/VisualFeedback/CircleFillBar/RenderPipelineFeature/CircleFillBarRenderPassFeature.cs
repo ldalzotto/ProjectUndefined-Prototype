7e357f1aea1d82e53dcf2e9c99f562bb2f678c6e
LDZ <loic.dalzotto@hotmail.fr>
2019-12-04 14:52:50 +0100

    GRAPH-005 - Upgrade to URP PostProcessing.

--- Assets/Common/VisualFeedback/CircleFillBar/RenderPipelineFeature/CircleFillBarRenderPassFeature.cs
+++ Assets/Common/VisualFeedback/CircleFillBar/RenderPipelineFeature/CircleFillBarRenderPassFeature.cs
@@ -31,7 +31,7 @@ namespace VisualFeedback
             m_ScriptablePass = new CustomRenderPass();
 
             // Configures where the render pass should be injected.
-            m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRendering;
+            m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
         }
 
         // Here you can inject one or multiple render passes in the renderer.
