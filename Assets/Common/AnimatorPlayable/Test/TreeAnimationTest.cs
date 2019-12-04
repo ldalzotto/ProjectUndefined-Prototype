735f076688c16135d39b055c1133d2f0cd1f58b6
LDZ <loic.dalzotto@hotmail.fr>
2019-12-04 09:22:11 +0100

    GRAPH-002 - Cleaning .blend files and assets

---
+++ Assets/Common/AnimatorPlayable/Test/TreeAnimationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AnimatorPlayable;
+
+namespace UnityEngine.Rendering
+{
+    public class TreeAnimationTest : MonoBehaviour
+    {
+        private AnimatorPlayableObject AnimatorPlayableObject;
+        public Vector2 Input;
+        public TwoDBlendTreePlayableDefinition TwoDBlendTreePlayableDefinition;
+
+        private void Start()
+        {
+            this.AnimatorPlayableObject = new AnimatorPlayableObject("Test", this.GetComponent<Animator>());
+            this.AnimatorPlayableObject.PlayAnimation(0, TwoDBlendTreePlayableDefinition.GetAnimationInput(), null, null, TwoDInputWheigtProvider);
+        }
+
+        private void Update()
+        {
+            this.AnimatorPlayableObject.Tick(Time.deltaTime);
+        }
+
+        private Vector2 TwoDInputWheigtProvider()
+        {
+            return this.Input;
+        }
+    }
+}
