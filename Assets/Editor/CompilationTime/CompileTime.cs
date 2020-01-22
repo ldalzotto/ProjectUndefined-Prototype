a9e029f84aabe56808cdde8bc3a516918b9e3f50
LDZ <loic.dalzotto@hotmail.fr>
2020-01-22 20:17:10 +0100

    Adding compile time script

---
+++ Assets/Editor/CompilationTime/CompileTime.cs
@@ -0,0 +1,41 @@
+﻿namespace CompilationTime
+{
+    using UnityEngine;
+    using UnityEditor;
+
+    [InitializeOnLoad]
+    class CompileTime : EditorWindow
+    {
+        static bool isTrackingTime;
+        static double startTime;
+
+        static CompileTime()
+        {
+            EditorApplication.update += Update;
+            startTime = PlayerPrefs.GetFloat("CompileStartTime", 0);
+            if (startTime > 0)
+            {
+                isTrackingTime = true;
+            }
+        }
+
+
+        static void Update()
+        {
+            if (EditorApplication.isCompiling && !isTrackingTime)
+            {
+                startTime = EditorApplication.timeSinceStartup;
+                PlayerPrefs.SetFloat("CompileStartTime", (float) startTime);
+                isTrackingTime = true;
+            }
+            else if (!EditorApplication.isCompiling && isTrackingTime)
+            {
+                var finishTime = EditorApplication.timeSinceStartup;
+                isTrackingTime = false;
+                var compileTime = finishTime - startTime;
+                PlayerPrefs.DeleteKey("CompileStartTime");
+                Debug.Log("Script compilation time: \n" + compileTime.ToString("0.000") + "s");
+            }
+        }
+    }
+}
